6acc2c65e0498c771cec97bf946429cdcfe3f4bc
gmiranda23 <gmiranda@opscode.com>
2015-07-19 00:27:04 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'gmiranda@chef.io'
 license 'MIT'
 description 'Installs/Configures chef_workstation'
 long_description 'Installs/Configures chef_workstation'
-version '0.1.0'
+version '0.1.1'
 
 supports 'centos'
+supports 'amazon'
 depends  "yum"
 depends  "chef-dk"
 depends  "sudo"
